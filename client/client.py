32360080098c0c991f1a8942ae56770c02750869
cadevu <carvalhocarlos233@gmail.com>
2023-06-11 19:30:39 -0300

    feat: implementing inital menu

--- client/client.py
+++ client/client.py
@@ -3,6 +3,32 @@ import threading
 import pyaudio
 import os
 import wave
+import json
+import pickle
+
+def list_devices(client_socket):
+    msg = {'service':'list_devices'}
+    msg_bytes = json.dumps(msg).encode('utf-8')
+    client_socket.send(msg_bytes)
+    devices = client_socket.recv(1024)
+    list_devices = pickle.loads(devices)
+    print(list_devices)
+
+def list_songs(client_socket):
+    msg = {'service':'list_songs'}
+    msg_bytes = json.dumps(msg).encode('utf-8')
+    client_socket.send(msg_bytes)
+    songs_list = client_socket.recv(2048).decode()
+    if not os.path.isdir("cache"):
+        os.makedirs("cache")
+    # Recuperar a lista de músicas do servidor
+    print("Lista de músicas disponíveis:")
+    print(songs_list)
+
+def choose_music():
+    pass
+
+    
 
 def play_audio(client_socket, song_choice):
     chunk_size = 1024
@@ -35,37 +61,39 @@ def play_audio(client_socket, song_choice):
 def start_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(("127.0.0.1", 10000))
-    if not os.path.isdir("cache"):
-        os.makedirs("cache")
-    songs_cache = os.listdir('cache') 
-
-    # Recuperar a lista de músicas do servidor
-    songs_list = client_socket.recv(2048).decode()
-    print("Lista de músicas disponíveis:")
-    print(songs_list)
-
-    # Escolher uma música para reproduzir
-    song_choice = input("Digite o nome da música que deseja reproduzir: ")
-    if song_choice in songs_cache:
-        chunk_size = 1024
-        p = pyaudio.PyAudio()
-        stream = p.open(format=p.get_format_from_width(3),
-                    channels=2,
-                    rate=44100,
-                    output=True)
-        print("Reproduzindo do cache!")
-        with open(f'cache/{song_choice}', 'rb') as file:
-            while True:
-                data = file.read(chunk_size)
-                buffer_size = len(data)  # Tamanho atual do buffer
-                print(f"Tamanho do buffer: {buffer_size}")
-                if not data:
-                    break
-                stream.write(data)
-        
-    else:
-        client_socket.send(song_choice.encode())
-        play_audio(client_socket, song_choice)
-        client_socket.close()
+    while True:
+        initial_command = input('1 - Listar dispositivos disponíveis\n2 - Listar músicas disponíveis\n3 - Tocar Música\n4 - Encerrar conexão\n')
+        match (initial_command):
+            case '1':
+                list_devices(client_socket)
+            case '2':
+                list_songs(client_socket)
+            case '3':
+                # Escolher uma música para reproduzir
+                song_choice = input("Digite o nome da música que deseja reproduzir: ")
+                songs_cache = os.listdir('cache')
+                if song_choice in songs_cache:
+                    chunk_size = 1024
+                    p = pyaudio.PyAudio()
+                    stream = p.open(format=p.get_format_from_width(2),
+                                    channels=2,
+                                    rate=44100,
+                                    output=True)
+                    print("Reproduzindo do cache!")
+                    with open(f'cache/{song_choice}', 'rb') as file:
+                        while True:
+                            data = file.read(chunk_size)
+                            buffer_size = len(data)  # Tamanho atual do buffer
+                            print(f"Tamanho do buffer: {buffer_size}")
+                            if not data:
+                                    break
+                            stream.write(data)
+                    
+                else:
+                    client_socket.send(song_choice.encode())
+                    play_audio(client_socket, song_choice)
+                    client_socket.close()
+            case _:
+                break;
 
 start_client()
--- server/server.py
+++ server/server.py
@@ -1,30 +1,115 @@
+# import socket
+# import threading
+# import os
+# import json
+# import pickle
+# disps = []
+
+# def handle_client(client_socket, client_address):
+#     print(f"Conexão estabelecida com o cliente {client_address}")
+
+#     #define serviço a ser feito
+#     """
+#      Padrão de mensagem:
+#      {service:'RECUPERAR MUSICAS'} 
+#      {service:'RECUPERAR DISPOSITIVOS'}
+#      {service: 'TOCAR MUSICA',dispositivo:'IP'}
+#      {'service':'PAUSAR MUSICA',dispositivo:'IP'}
+     
+#      """
+#     initial_option = client_socket.recv(1024).decode()
+#     msg_json = json.loads(initial_option)
+#     print(msg_json)
+
+#     if msg_json['SERVICO'] == "LISTAR DISPOSITIVOS":
+#         print('here')
+#         client_socket.send(pickle.dumps(disps))
+#     if msg_json['SERVICO'] == 'LISTAR MUSICAS':
+
+#     #Recuperar a lista de músicas disponíveis no servidor
+#         songs = os.listdir('resource')
+#         songs = [song for song in songs if song.endswith(".wav")]
+#         songs_str = "\n".join(songs)
+#         client_socket.send(songs_str.encode())
+
+#         # Receber a música escolhida pelo cliente
+#         song_choice = client_socket.recv(1024).decode()
+
+#         # Verificar se a música está presente no cache local
+#         if os.path.exists(f"cache/{song_choice}"):
+#             client_socket.send("OK".encode())
+#         else:
+#             client_socket.send("NOT FOUND".encode())
+
+#         # Enviar a música para o cliente em blocos de 30 segundos
+#         if os.path.exists(f"resource/{song_choice}"):
+#             with open(f"resource/{song_choice}", "rb") as song_file:
+#                 data = song_file.read(1024)
+#                 while data:
+#                     client_socket.send(data)
+#                     data = song_file.read(1024)
+
+#     # Fechar a conexão com o cliente
+#     client_socket.close()
+
+#     print(f"Conexão encerrada com o cliente {client_address}")
+
+# def start_server():
+#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     server_socket.bind(("127.0.0.1", 10000))
+#     server_socket.listen(5)
+
+#     print("Servidor iniciado. Aguardando conexões...")
+
+#     while True:
+#         client_socket, client_address = server_socket.accept()
+#         disps.append(client_address)
+#         print(disps)
+#         client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+#         client_thread.start()
+
+# start_server()
+
 import socket
 import os
 import wave
 from _thread import *
+import json
+import pickle
+devices = []
 
 def handle_client(client_socket, client_address):
     print(f"Conexão estabelecida com o cliente {client_address}")
 
     while True:
+
         # Recuperar a lista de músicas disponíveis no servidor
-        songs = os.listdir("resource")
+        songs = os.listdir('resource')
         songs = [song for song in songs if song.endswith(".wav")]
         songs_str = "\n".join(songs)
-        client_socket.send(songs_str.encode())
-        # Receber a música escolhida pelo cliente
+        initial_cmd = client_socket.recv(1024).decode()
+        request = json.loads(initial_cmd)
+        print(request)
+        if request['service'] == 'list_devices':
+            print('devices')
+            client_socket.send(pickle.dumps(devices))
+        if request['service'] == 'list_songs':
+            print('songs')
+            client_socket.send(songs_str.encode())
+            # Receber a música escolhida pelo cliente
+            song_choice = client_socket.recv(1024).decode()
 
-        song_choice = client_socket.recv(1024).decode()
-        if os.path.exists(f"resource/{song_choice}"):
-            with wave.open(f"resource/{song_choice}", "rb") as song_file:
-                data = 1
-                while data != b'':
-                    data = song_file.readframes(1024)
-                    client_socket.send(data)
-                    print(data)
-                    print("Deseja continuar?")
-                break
+            if os.path.exists(f"resource/{song_choice}"):
+                with wave.open(f"resource/{song_choice}", "rb") as song_file:
+                    data = 1
+                    while data != b'':
+                        data = song_file.readframes(1024)
+                        client_socket.send(data)
+                        # print(data)
+                        # print("Deseja continuar?")
+                    break
     client_socket.close()
+    devices.remove(client_address)
     print(f"Conexão encerrada com o cliente {client_address}")
 
                 
@@ -38,6 +123,7 @@ def start_server():
 
     while True:
         client_socket, client_address = server_socket.accept()
+        devices.append(client_address)
         start_new_thread(handle_client,(client_socket, client_address))
 
 start_server()
